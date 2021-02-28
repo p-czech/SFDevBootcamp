09c9b3016ddcfb7704d525b14068d4308788b117
Czech <p.czech93@gmail.com>
2021-02-28 13:51:50 +0100

    expenses app to show components interaction

---
+++ src/aura/expenseItem/expenseItemController.js
@@ -0,0 +1,11 @@
+({
+    doInit : function(component, event, helper) {
+        let mydate = component.get("v.expense.Date__c");
+        if(mydate){
+            component.set("v.formatdate", new Date(mydate));
+        }
+    },
+    clickReimbursed: function () {
+        
+    }
+})
