037523cf1c2027493cb7378dee30c4052a7fac2c
Czech <p.czech93@gmail.com>
2021-03-02 00:33:45 +0100

    saving camping items to  DB using Apex Controller

--- src/aura/campingList/campingListController.js
+++ src/aura/campingList/campingListController.js
@@ -1,18 +1,25 @@
 ({
+    doInit : function (component, event, helper) {
+        let action = component.get('c.getItems');
+        action.setCallback(this, function(response) {
+            console.log('doInitFired');
+            let state = response.getState();
+            if(state === 'SUCCESS') {
+                component.set('v.items', response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+
+    },
+
     clickCreateItem : function (component, event, helper) {
         let formValid = component.find('campingitemform').reduce(function(validStr, inputComp) {
             return validStr && inputComp.get('v.validity').valid;
         }, true);
+        
         if(formValid) {
-            let newItem = component.get('v.newItem');
-            let items = component.get('v.items');
-            items.push(newItem);
-            component.set('v.items', items);
-            component.set('v.newItem', { 'sObjectType' : 'Camping_Item__c',
-            'Name' : '',
-            'Quantity__c' : '0',
-            'Price__c' : '0',
-            'Packed__c' : 'false'});
+            let item = component.get('v.newItem');
+            helper.createItem(component, item);
         }
     }
 })
