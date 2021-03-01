52eb5d1c9fa168e4dba4c89d298dfcbe7007712f
Czech <p.czech93@gmail.com>
2021-03-01 23:10:40 +0100

    added Apex Controller to complete app

--- src/aura/expenses/expensesController.js
+++ src/aura/expenses/expensesController.js
@@ -1,9 +1,9 @@
 ({
     clickCreate: function(component, event, helper) {
-        let validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
+        let validExpense = component.find("expenseform").reduce(function (validSoFar, inputCmp) {
             // Displays error messages for invalid fields
             inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
+            return validSoFar && inputCmp.get("v.validity").valid;
         }, true);
         // If we pass error checking, do some real work
         if(validExpense){
@@ -12,5 +12,18 @@
             console.log("Create expense: " + JSON.stringify(newExpense));
             helper.createExpense(component, newExpense);
         }
+    },
+    doInit : function (component, event, helper) {
+        let action = component.get("c.getExpenses");
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if(state === "SUCCESS"){
+                component.set("v.expenses", response.getReturnValue());
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
--- src/aura/expenses/expensesHelper.js
+++ src/aura/expenses/expensesHelper.js
@@ -1,10 +1,18 @@
 ({
     createExpense: function(component, expense) {
-        let theExpenses = component.get("v.expenses");
-        // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        let newExpense = JSON.parse(JSON.stringify(expense));
-        theExpenses.push(newExpense);
-        component.set("v.expenses", theExpenses);
+       let action = component.get("c.saveExpense");
+       action.setParams({
+           "expense":expense
+       });
+    
+       action.setCallback(this, function(response){
+           let state = response.getState();
+           if(state === "SUCCESS") {
+               let expenses = component.get("v.expenses");
+               expenses.push(response.getReturnValue());
+               component.set("v.expenses", expenses);
+           }
+       });
+       $A.enqueueAction(action);
     }
 })
